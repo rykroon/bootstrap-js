7f930d5a6b9f050c0434ca0a02e7ee198047caf0
Ryan <rynkrn321@gmail.com>
2019-05-04 12:41:14 -0400

    created logic for Containers, Rows, and Columns

---
+++ tests/app.js
@@ -0,0 +1,242 @@
+//test
+
+
+class Container extends HTMLDivElement {
+
+	constructor() {
+		super();		
+		this.className='container';
+	}
+	
+	get fluid() {
+		return this.classList.contains('container-fluid');
+	}
+
+	set fluid(value) {
+		if (value) {
+			this.classList.add('container-fluid');
+		} else {
+			this.classList.remove('container-fluid');
+		}
+	}
+
+
+	appendToBody() {
+		let body = document.getElementsByTagName('body')[0];
+		if (body) {
+			body.appendChild(this);
+		}
+	}
+}
+
+class Row extends HTMLDivElement {
+	constructor() {
+		super();
+		this.className='row';
+	}
+}
+
+class Col extends HTMLDivElement {
+	constructor() {
+		super();
+
+		this._breakpoints = [undefined, 'sm', 'md', 'lg', 'xl']
+
+		this._attributes = {
+			'col': {
+				'classes': {
+					undefined: null,
+					'sm': null,
+					'md': null,
+					'lg': null,
+					'xl': null
+				},
+				values: [undefined, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+			},
+			'order': {
+				'classes': {
+					undefined: null,
+					'sm': null,
+					'md': null,
+					'lg': null,
+					'xl': null
+				},
+				values: ['first', 'last', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+			},
+			'offset': {
+				'classes': {
+					undefined: null,
+					'sm': null,
+					'md': null,
+					'lg': null,
+					'xl': null
+				},
+				values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+			}
+		}
+
+		this.extraSmall();
+	}
+
+	_addAttribute(attr, bp, value) {
+		//check for valid attribute
+		if (! attr in this._attributes) throw "Invalid attribute";
+
+		//Check for valid breakpoint
+		if (!this._breakpoints.includes(bp)) throw "Invalid breakpoint";
+
+		//check for valid value
+		let values = this._attributes[attr]['values'];
+		if (!values.includes(value)) throw "Invalid value";
+
+		//build className
+		let className = attr;
+		if (bp !== undefined) className += '-' + bp;
+		if (value !== undefined) className += '-' + value;
+
+		this.classList.add(className);
+		this._attributes[attr]['classes'][bp] = className;
+	}
+
+	_removeAttribute(attr, bp) {
+		//Check for valid attribute
+		if (! attr in this._attributes) throw "Invalid attribute";
+
+		//Check for valid breakpoint
+		if (!this._breakpoints.includes(bp)) throw "Invalid breakpoint";
+
+		//get current className
+		let className = this._attributes[attr]['classes'][bp];
+
+		this.classList.remove(className);
+		this._attributes[attr]['classes'][bp] = null;
+	}
+
+	_updateAttribute(attr, bp, value) {
+		this._removeAttribute(attr, bp);
+
+		if (value !== null) {
+			this._addAttribute(attr, bp, value);
+		}
+	}
+
+	//Column Classes
+	col(value, bp) {
+		const attr = 'col';
+		this._updateAttribute(attr, bp, value);
+		return this;
+	}
+
+	extraSmall(value) {
+		const bp = undefined;
+		return this.col(value, bp);
+	}
+
+	small(value) {
+		const bp = 'sm';
+		return this.col(value, bp);
+	}
+
+	medium(value) {
+		const bp = 'md';
+		return this.col(value, bp);
+	}
+
+	large(value) {
+		const bp = 'lg';
+		return this.col(value, bp);
+	}
+
+	extraLarge(value) {
+		const bp = 'xl';
+		return this.col(value, bp);
+	}
+
+	// Order Classes
+
+	order(value, bp) {
+		const attr = 'order';
+		this._updateAttribute(attr, bp, value);
+		return this;
+	}
+
+	orderSmall(value) {
+		const bp = 'sm';
+		return this.order(value, bp);
+	}
+
+	orderMedium(value) {
+		const bp = 'md';
+		return this.order(value, bp);
+	}
+
+	orderLarge(value) {
+		const bp = 'lg';
+		return this.order(value, bp);
+	}
+
+	orderExtraLarge(value) {
+		const bp = 'xl';
+		return this.order(value, bp);
+	}
+
+	// Offset
+
+	offset(value) {
+		const attr = 'offset';
+		const bp = undefined;
+		return this._updateAttribute(attr, bp, value);
+	}
+
+	offsetSmall(value) {
+		const attr = 'offset';
+		const bp = 'sm';
+		return this._updateAttribute(attr, bp, value);
+	}
+
+	offsetMedium(value) {
+		const attr = 'offset';
+		const bp = 'md';
+		return this._updateAttribute(attr, bp, value);
+	}
+
+	offsetLarge(value) {
+		const attr = 'offset';
+		const bp = 'lg';
+		return this._updateAttribute(attr, bp, value);
+	}
+
+	offsetExtraLarge(value) {
+		const attr = 'offset';
+		const bp = 'xl';
+		return this._updateAttribute(attr, bp, value);
+	}
+}
+
+window.customElements.define('bs-container', Container, {extends: 'div'});
+window.customElements.define('bs-row', Row, {extends: 'div'});
+window.customElements.define('bs-col', Col, {extends: 'div'});
+
+container = new Container();
+row = new Row();
+
+col1 = new Col();
+col1.classList.add('bg-primary');
+col1.classList.add('p-5');
+
+col2 = new Col();
+col2.classList.add('bg-secondary');
+col2.classList.add('p-5');
+
+col3 = new Col();
+col3.classList.add('bg-success');
+col3.classList.add('p-5');
+
+row.appendChild(col1);
+row.appendChild(col2)
+row.appendChild(col3)
+container.appendChild(row)
+container.appendToBody();
+
+
+
