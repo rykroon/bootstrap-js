43d26a66bfa45adcb6884b9af2b269bec12bfc94
Ryan <rynkrn321@gmail.com>
2019-05-07 07:09:46 -0400

    removed order methods from columns and justify/align methods from rows since they exist in the flex mixin

--- src/grid.js
+++ src/grid.js
@@ -33,15 +33,9 @@ class Row extends HTMLDivElement {
 	constructor() {
 		super();
 		this.className='row';
-
-		this._responsiveProperties = {
-			'justify-content': ['start', 'center', 'end', 'around', 'between'],
-			'align-items': ['start', 'end', 'center', 'baseline', 'stretch']	
-        };
 	}
 
 	//No Gutters
-
 	get noGutters() {
 		return this.classList.contains('no-gutters');
 	}
@@ -53,20 +47,6 @@ class Row extends HTMLDivElement {
 			this.classList.remove('no-gutters');
 		}
     }
-    
-	//Justify Content
-	justify(value, bp)          {return this._updateResponsiveProperty('justify-content', bp, value);}
-	justifySmall(value)         {return this.justify(value, 'sm');}
-	justifyMedium(value)        {return this.justify(value, 'md');}
-	justifyLarge(value)         {return this.justify(value, 'lg');}
-	justifyExtraLarge(value)    {return this.justify(value, 'xl');}
-
-	//Align Items
-	align(value, bp)        {return this._updateResponsiveProperty('align-items', bp, value);}
-	alignSmall(value)       {return this.align(value, 'sm');}
-	alignMedium(value)      {return this.align(value, 'md');}
-	alignLarge(value)       {return this.align(value, 'lg');}
-	alignExtraLarge(value)  {return this.align(value, 'xl');}
 }
 
 
@@ -76,7 +56,6 @@ class Col extends HTMLDivElement {
 
 		this._responsiveProperties = {
 			col: [undefined, 'auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-			order: ['first', 'last', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 			offset: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         }
 
@@ -89,13 +68,6 @@ class Col extends HTMLDivElement {
 	medium(value)       {return this.col(value, 'md');}
 	large(value)        {return this.col(value, 'lg');}
 	extraLarge(value)   {return this.col(value, 'xl');}
-
-	// Order Classes
-	order(value, bp)        {return this._updateResponsiveProperty('order', bp, value);}    
-	orderSmall(value)       {return this.order(value, 'sm');}
-	orderMedium(value)      {return this.order(value, 'md');}
-	orderLarge(value)       {return this.order(value, 'lg');}
-	orderExtraLarge(value)  {return this.order(value, 'xl');}
 
 	// Offset
 	offset(value, bp)       {return this._updateResponsiveProperty('offset', bp, value);}
