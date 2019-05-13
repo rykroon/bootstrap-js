033b552893fef757fdaa3519106683b0e08d3b00
Ryan <rynkrn321@gmail.com>
2019-05-12 20:50:09 -0400

    started working on a new button class

--- src/components.js
+++ src/components.js
@@ -156,4 +156,50 @@ class Card extends HTMLDivElement {
 
 }
 
+class Btn extends HTMLButtonElement {
+    constructor() {
+        super();
+        this.className = 'btn'; 
+    }
+
+    get large() {
+        return this.classList.contains('btn-lg');
+    }
+
+    set large(value) {
+        if (value) {
+            this.classList.remove('btn-sm');
+            this.classList.add('btn-lg');
+        } else {
+            this.classList.remove('btn-lg');
+        }
+    }
+
+    get small() {
+        return this.classList.contains('btn-sm');
+    }
+
+    set small(value) {
+        if (value) {
+            this.classList.remove('btn-lg');
+            this.classList.add('btn-sm');
+        } else {
+            this.classList.remove('btn-sm');
+        }
+    }
+
+    get block() {
+        return this.classList.contains('btn-block');
+    }
+
+    set block(value) {
+        if (value) {
+            this.classList.add('btn-block');
+        } else {
+            this.classList.remove('btn-block');
+        }
+    }
+}
+
+window.customElements.define('bs-button', Btn, {extends: 'button'});
 window.customElements.define('bs-card', Card, {extends: 'div'});
