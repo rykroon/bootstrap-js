baf8424f76c87a67ffc1c8079a933562844e9ac1
Ryan <rynkrn321@gmail.com>
2019-05-18 09:23:22 -0400

    added outline colors and disable/enable functions to buttons

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,56 +1,137 @@
 class Btn extends HTMLButtonElement {
-    constructor() {
+    constructor(text) {
         super();
         this.className = 'btn'; 
         this.primary();
+        this.text(text)
     }
 
-    get large() {
-        return this.classList.contains('btn-lg');
+    text(value) {
+        this.textContent = value;
+        return this;
     }
 
-    set large(value) {
-        if (value) {
-            this.small = false;
+    lg(value) {
+        if (value || value === undefined) {
+            this.classList.remove('btn-sm');
             this.classList.add('btn-lg');
         } else {
             this.classList.remove('btn-lg');
         }
+
+        return this;
     }
 
-    get small() {
-        return this.classList.contains('btn-sm');
-    }
-
-    set small(value) {
-        if (value) {
-            this.large = false;
+    sm(value) {
+        if (value || value === undefined) {
+            this.classList.remove('btn-lg');
             this.classList.add('btn-sm');
         } else {
             this.classList.remove('btn-sm');
         }
+
+        return this;
     }
 
-    get block() {
-        return this.classList.contains('btn-block');
-    }
-
-    set block(value) {
-        if (value) {
+    block(value) {
+        if (value || value === undefined) {
             this.classList.add('btn-block');
         } else {
             this.classList.remove('btn-block');
         }
+
+        return this;
     }
 
-    primary()   {return this._primary('btn')}
-    secondary() {return this._secondary('btn')}
-    success()   {return this._success('btn')}
-    info()      {return this._info('btn')}
-    warning()   {return this._warning('btn')}
-    danger()    {return this._danger('btn')}
-    light()     {return this._light('btn')}
-    dark()      {return this._dark('btn')}
+    primary()   {
+        this._removePropertyColor('btn-outline');
+        return this._primary('btn');
+    }
+
+    secondary() {
+        this._removePropertyColor('btn-outline');
+        return this._secondary('btn');
+    }
+
+    success() {
+        this._removePropertyColor('btn-outline');
+        return this._success('btn');
+    }
+
+    info() {
+        this._removePropertyColor('btn-outline');
+        return this._info('btn');
+    }
+
+    warning() {
+        this._removePropertyColor('btn-outline');
+        return this._warning('btn');
+    }
+
+    danger() {
+        this._removePropertyColor('btn-outline');
+        return this._danger('btn');
+    }
+
+    light() {
+        this._removePropertyColor('btn-outline');
+        return this._light('btn');
+    }
+
+    dark() {
+        this._removePropertyColor('btn-outline');
+        return this._dark('btn');
+    }
+
+    outlinePrimary() {
+        this._removePropertyColor('btn');
+        return this._primary('btn-outline');
+    }
+
+    outlineSecondary() {
+        this._removePropertyColor('btn');
+        return this._secondary('btn-outline');
+    }
+
+    outlineSuccess() {
+        this._removePropertyColor('btn');
+        return this._success('btn-outline');
+    }
+
+    outlineInfo() {
+        this._removePropertyColor('btn');
+        return this._info('btn-outline');
+    }
+
+    outlineWarning() {
+        this._removePropertyColor('btn');
+        return this._warning('btn-outline');
+    }
+
+    outlineDanger() {
+        this._removePropertyColor('btn');
+        return this._danger('btn-outline');
+    }
+
+    outlineLight() {
+        this._removePropertyColor('btn');
+        return this._light('btn-outline');
+    }
+
+    outlineDark() {
+        this._removePropertyColor('btn');
+        return this._dark('btn-outline');
+    }
+
+    disable() {
+        this.disabled = true;
+        return this;
+    }
+
+    enable() {
+        this.disabled = false;
+        return this;
+    }
 }
 
 window.customElements.define('bs-button', Btn, {extends: 'button'});
