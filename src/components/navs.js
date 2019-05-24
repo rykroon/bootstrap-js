d7baf74018541dd71ec7546ac270a23349fea5a3
Ryan <rynkrn321@gmail.com>
2019-05-24 09:33:12 -0400

    Added new Nav component

---
+++ src/components/navs.js
@@ -0,0 +1,77 @@
+
+class Nav extends HTMLUListElement {
+    constructor() {
+        super();
+        this.classList.add('nav');
+    }
+
+    newItem(text, href) {
+        let navItem = new NavItem(text, href);
+        this.appendChild(navItem);
+        return navItem;
+    }
+
+    tabs(value) {
+        return this._updateClass(value, 'nav-tabs');
+    }
+
+    pills(value) {
+        return this._updateClass(value, 'nav-pills');
+    }
+
+    fill(value) {
+        return this._updateClass(value, 'nav-fill');
+    }
+
+    justified(value) {
+        return this._updateClass(value, 'nav-justified');
+    }
+
+}
+
+
+class NavItem extends HTMLLIElement {
+    constructor(text, href) {
+        super();
+        this.classList.add('nav-item');
+        this.link = new NavLink(text, href);
+        this.appendChild(this.link);
+    }
+
+    newItemBefore(text, href) {
+        let navItem = new NavItem(text, href);
+        this.before(navItem);
+        return navItem;
+    }
+
+    newItemAfter(text, href) {
+        let navItem = new NavItem(text, href);
+        this.after(navItem);
+        return navItem;
+    }
+
+    active(value) {
+        this.link._updateClass(value, 'active');
+        return this;
+    }
+}
+
+
+class NavLink extends HTMLAnchorElement {
+    constructor(text, href) {
+        super();
+        this.classList.add('nav-link');
+
+        if (text) this.textContent = text;
+        if (href) {
+            this.href = href;
+        } else {
+            this.href = '#';
+        }
+    }
+}
+
+
+window.customElements.define('bs-nav', Nav, {extends: 'ul'});
+window.customElements.define('bs-nav-item', NavItem, {extends: 'li'});
+window.customElements.define('bs-nav-link', NavLink, {extends: 'a'});
